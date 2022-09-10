1d2c7b1e141b25ebe1a43f3da020a9dfff05e87b
pujisudaryanto <sudaryanto200@gmail.com>
2022-09-11 03:03:17 +0700

    Kazee App

--- public/js/sideMenu.js
+++ public/js/sideMenu.js
@@ -11,25 +11,35 @@ var sideMenu = document.getElementById('side-menu');
 
 var sideUrutan = document.getElementById('side-urutan');
         function openUrutan(){
-            sideUrutan.classList.remove('bottom-[-300px]');
+            sideUrutan.classList.remove('bottom-[-350px]');
             sideUrutan.classList.add('bottom-0');
         }
 
         function closeUrutan(){
             sideUrutan.classList.remove('bottom-0');
-            sideUrutan.classList.add('bottom-[-300px]');
+            sideUrutan.classList.add('bottom-[-350px]');
+        }
+var sideRentan = document.getElementById('side-rentan');
+        function openRentan(){
+            sideRentan.classList.remove('bottom-[-380px]');
+            sideRentan.classList.add('bottom-0');
+        }
+
+        function closeRentan(){
+            sideRentan.classList.remove('bottom-0');
+            sideRentan.classList.add('bottom-[-380px]');
         }
 
 
 var sideFilter = document.getElementById('side-filter');
         function openFilter(){
-            sideFilter.classList.remove('bottom-[-920px]');
-            sideFilter.classList.add('-bottom-[80px]');
+            sideFilter.classList.remove('bottom-[-740px]');
+            sideFilter.classList.add('-bottom-0');
         }
 
         function closeFilter(){
-            sideFilter.classList.remove('-bottom-[80px]');
-            sideFilter.classList.add('bottom-[-920px]');
+            sideFilter.classList.remove('-bottom-0');
+            sideFilter.classList.add('bottom-[-740px]');
         }
 
 var sideKustom = document.getElementById('side-kustom');
@@ -58,4 +68,34 @@ var sideKustom = document.getElementById('side-kustom');
         function aturUlang() {
             document.getElementById("form").reset();
         }
+        function aturUlang1() {
+            document.getElementById("form1").reset();
+        }
+        // delay on link page
+        function delay(){
+            setTimeout(function(){ 
+            window.location="linimasa.html"
+         }, 1000);
+        }
+        // tombol loading
+        const btn = document.getElementById('loading');
+            btn.addEventListener('click', function handleClick() {
+            btn.textContent = 'Menyimpan...';
+        });
+
+
+        //hapus semua checkbox
+        function uncek() {
+            var inputs = document.querySelectorAll('.check');
+            for(var i = 0; i < inputs.length; i++) {
+              inputs[i].checked = false;
+            }
+          }
+          
+          window.onload = function() {
+            window.addEventListener('load', uncek, false);
+          }
         
+
+
+
